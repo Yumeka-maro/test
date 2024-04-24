3fb70b4a6a111dacad472198f3900609b117d011
‘Maro’ <ytya4496@gmail.comgit config --global user.name Yumeka-maro/livecording03git config --global user.email ytya4496@gmail.com>
2024-04-25 00:15:10 +0900

    0424

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,27 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+    //ドロワーメニュー
+    $(".js-hamburger").click(function () {
+    if ($(".js-hamburger").hasClass('is-active')){
+        $('.js-hamburger').removeClass("is-active");
+      //$("html").toggleClass("is-fixed");
+        $(".js-sp-nav").fadeOut();
+    } else {
+        $('.js-hamburger').addClass("is-active");
+      //$("html").toggleClass("is-fixed");
+        $(".js-sp-nav").fadeIn();
+    }
+    });
 
+    //スライダー
+    const swiper = new Swiper(".swiper", {
+    loop: true,
+    effect: "fade",
+    speed: 3000,
+    allowTouchMove: false,
+    autoplay: {
+    delay: 3000,
+    },
+});
 
 });
